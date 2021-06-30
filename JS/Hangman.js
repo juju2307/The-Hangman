2661a74d932af05b3735a2031414259026f34dfe
juju2307 <metzjulie2@gmail.com>
2021-06-30 12:08:50 +0200

    suite du JS et README

--- JS/Hangman.js
+++ JS/Hangman.js
@@ -32,6 +32,15 @@ let motscaches = [
 //let letter = readlineSync.question("Can you put a single letter on the keywoard please?");
 //demander de choisir une lettre via le clavier
 
+document.getElementById("1").style.display = "none";
+document.getElementById("2").style.display = "none";
+document.getElementById("3").style.display = "none";
+document.getElementById("4").style.display = "none";
+document.getElementById("5").style.display = "none";
+document.getElementById("6").style.display = "none";
+document.getElementById("7").style.display = "none";
+document.getElementById("8").style.display = "none";
+
 let wordToGuess = motscaches[Math.floor(Math.random() * motscaches.length)];
 //permet de donner un mot aléatoire parmis la liste
 
@@ -43,10 +52,41 @@ let wordCurrent = "-";
 
 let lettersCurrent = [];
 //variable qui stocke la lettre actuelle
+ let lettersWrong = [];
+
+ let wrong = "";
 
 window.onload = function(){
     console.log(wordToGuess)
     }
+//fonction qui permet de donner un nouveau mot à l'ouverture de la fenêtre
+    function ChangeCurrentWord() {
+        wordCurrent = "";
+        for (x=0;x<lettersCurrent.length;x++){
+            wordCurrent = wordCurrent + lettersCurrent[x] + " ";
+        }
+        document.getElementById("underscore").innerHTML = wordCurrent;
+    };
+    //fonction pour apporter les nouvelles lettres au mot de départ
+
+    function changeLetterWrong() {
+        wrong ="";
+        for (x=0; x < lettersWrong.length;x++) {
+            wrong = wrong + lettersWrong[x] + " ";
+        }
+    }
+
+    for  (x=0; x< letterToGuess.length; x++) {
+        if (letterToGuess[x] == " ") {
+            lettersCurrent.push [" "];
+        }
+        else {
+            lettersCurrent.push ("_");
+        }
+    };
+     document.getElementById("underscore").innerHTML= wordCurrent;
+
+
 
 //creation du clavier
 let lettres = document.getElementsByClassName("lettre") ;
@@ -56,61 +96,78 @@ let lettresArray = document.querySelectorAll(".lettre");
 lettresArray.forEach(
     function (lettre) {
         lettre.addEventListener("click",function(event) {
-          console.log(event.target.innerHTML)
-          for (x=0; x<letterToGuess.length; x++) {
-             if (letterToGuess.includes(event.target.innerHTML)=== true) {
-                 lettersCurrent.push (""); 
-                   
-              //permet de savoir si la lettre proposée fait partie du mot à découvrir
-            }else {
-                  lettersCurrent.push ("_");
-                  console.log(lettersCurrent)
-    }
-       }
-        })
-        
-    }
+          console.log(event.target.innerHTML);
+
+           if (letterToGuess.includes(event.target.innerHTML)=== true) {
+
+                for (x=0; x<letterToGuess.length; x++) {
+
+                    if (letterToGuess[x] === event.target.innerHTML) {
+                 lettersCurrent[x] = event.target.innerHTML;
+                   ChangeCurrentWord();
+               }
+            }
+           
+            }
+            else {
+                  lettersWrong.push (event.target.innerHTML);
+                 changeLetterWrong();
+                 document.getElementById("letterUsed").innerHTML = wrong;
+
+                 if (lettersWrong.length == 1) {
+                     document.getElementById("1").style.display = "block";
+                 }
+                 if (lettersWrong.length == 2) {
+                     document.getElementById("1").style.display = "none";
+                     document.getElementById("2").style.display = "block";
+                 }
+                 if (lettersWrong.length == 3) {
+                     document.getElementById("2").style.display = "none";
+                     document.getElementById("3").style.display = "block";
+                 }
+                 if (lettersWrong.length == 4) {
+                     document.getElementById("3").style.display = "none";
+                     document.getElementById("4").style.display = "block";
+                 }
+                 if (lettersWrong.length == 5) {
+                     document.getElementById("4").style.display = "none";
+                     document.getElementById("5").style.display = "block";
+                 }
+                 if (lettersWrong.length == 6) {
+                     document.getElementById("5").style.display = "none";
+                     document.getElementById("6").style.display = "block";
+                 }
+                 if (lettersWrong.length == 7) {
+                     document.getElementById("6").style.display = "none";
+                     document.getElementById("7").style.display = "block";
+                 }
+                 if (lettersWrong.length == 8) {
+                     document.getElementById("7").style.display = "none";
+                     document.getElementById("8").style.display = "block";
+                 }
+                 }
+           
+        });
+   }
 )
+//fonction qui permet de savoir si la lettre proposée fait partie du mot à découvrir
+// si oui alors la lettre est push
+// si non alors on push un underscore
 
-function ChangeCurrentWord() {
-    wordCurrent = "";
-    for (x=0;x<lettersCurrent.length;x++){
-        wordCurrent = wordCurrent + lettersCurrent[x] + " ";
-        
-    }
-}
-//fonction pour apporter les nouvelles lettres au mot de départ
+function YouAreTheWinner(){
+     
+         if (letterToGuess.length === wordCurrent.length) {
+             return "Vous avez gagné! Félicitations!";
+         }
+};
+//fonction qui place une alert pour dire qu'il a gagné (mais problème de fonctionnement)
 
-var WrongLetters = [];
-//Letters previously guessed but incorrect
-
-var Wrong = "";
-//Wrong letters, for displaying purposes
-
-
-
-
-    
-    
-
+console.log(YouAreTheWinner());
 
 function  nouveauJeuBoutton () {
-  
-   
-  
+  window.location.reload();
 }
-
-
-
-
 //création de la fonction pour actionner le boutton NEWGAME
-
-
-
-
-
-
-//fonction pour changer le mot courant 
 
 
 function putLetter(letter,) {
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# The-Hangman
+# The-Hangman-Game
+
+
+## Description du projet
+ Création d'un jeu du pendu. Un mot caché est donner de façon aléatoire.  L'utilisateur doit le découvrir en max 8 chances.   Il doit pour se faire cliquer sur une lettre de l'alphabet.  Si la lettre appartient bien au mot à découvrir, celle-ci sera placée au bonne endroit dans le mot.  Si par contre elle n'appartient pas au mot à découvrir alors une image apparaitra et ce jusqu'à ce que le bonhomme soit complètement pendu.  
+
